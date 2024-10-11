494d5ac8dad42c88291ab81a2461a532c0c64f83
Fatemeh <f.khalleghi.777@gmail.com>
2024-10-11 22:37:47 +0200

    if throw

--- src/main/java/se/lexicon/MathOperations.java
+++ src/main/java/se/lexicon/MathOperations.java
@@ -25,6 +25,9 @@ public class MathOperations {
         return multi;
     }
     public static int divide(int num1 , int num2){
+        if(num2==0){
+            throw new ArithmeticException("Divide by zero");
+        }
         return num1 / num2;
     }
 }
